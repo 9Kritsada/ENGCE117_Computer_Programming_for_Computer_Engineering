9073afe71e6b0b442ae7a8267ed182cb6b0c5734
9Kritsada <kritsada.wiriya@gmail.com>
2023-02-18 12:46:18 +0700

    :bug: Fix : bug

--- Lab11/Code11_01.cpp
+++ Lab11/Code11_01.cpp
@@ -4,19 +4,21 @@ int BinSearch( int data[], int n, int find ) ;
 
 int main() {
   int data[ 6 ] = { 1, 2, 3, 4, 5, 7 } ;
-  int n, find = 5 ;
+  int n = 6 , find = 5 ;
   int pos = BinSearch( data, n, find ) ;
   printf("Found %d at %d", find, pos ) ;
   return 0 ;
 }//end function
 
-int BinSearch( int data[], int n, int find ) {
-  int i, pos = -1;
-  for( i = 0 ; i < n ; i++ ) {
-    if( data[i] == find ) {
-      pos = i ;
-      break;
-    }
+int BinSearch( int data[], int last, int find ) {
+  int i = 0; 
+  int j = last - 1 ;
+  int pos, m ;
+  while( i < j ) {
+    m = ( i + j ) / 2 ;
+    if( data[ m ] < find ) i = m + 1 ;
+    else j = m ;
   }
+  pos = ( data[ i ] == find ? i : -1 ) ;
   return pos ;
 }
